d6c3fe29622ac0d88f5aeafefe1e8e7c62ff217a
Daniel Otieno <danielotieno.ke@gmail.com>
2019-09-18 12:30:03 +0300

    chore(fetch):get sample chicken data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "dotenv/config";
 
 function App() {
+  useEffect(() => {
+    data();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { APP_ID, APP_KEY } = process.env;
+
+  const data = async () => {
+    const sampleData = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+
+    const fetchedData = await sampleData.json();
+    console.log(fetchedData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <form>
+        <input type="search" />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
