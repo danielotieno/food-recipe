7c515aa7b3330f4ac29d4dc422eff6d7c8f930f7
Daniel Otieno <danielotieno.ke@gmail.com>
2019-09-19 14:27:53 +0300

    Updates

---
+++ precache-manifest.3b5a092c1b636c663d15990853c61edf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3c0ff8072bc8052a924880336b61b2",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "2f371983df4b18f44471",
+    "url": "/food-recipe/static/css/main.f57e98a6.chunk.css"
+  },
+  {
+    "revision": "ef36ee2604610dc82399",
+    "url": "/food-recipe/static/js/2.21bf212b.chunk.js"
+  },
+  {
+    "revision": "2f371983df4b18f44471",
+    "url": "/food-recipe/static/js/main.804fbc57.chunk.js"
+  },
+  {
+    "revision": "7ff7a1efe2b3ff6e07c0",
+    "url": "/food-recipe/static/js/runtime~main.f99d16f5.js"
+  }
+]);
